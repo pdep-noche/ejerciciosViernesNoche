24d0f53705e789a6a5c7afdebe8afe5b8396cf47
vpennella <vpennella@gmail.com>
2020-04-24 21:41:21 -0300

    ejercicio 2

--- tiposPropiosOrdenSuperior.hs
+++ tiposPropiosOrdenSuperior.hs
@@ -12,3 +12,26 @@ juan = Jefe "juan" 20000 4
 
 ana :: Empleado
 ana = Comun "ana" 25000
+
+type Nombre = String
+type Azucar = Int
+type Sabor = String
+data Bebida = Cafe Nombre Azucar | Gaseosa Sabor Azucar
+
+esEnergizante :: Bebida -> Bool
+esEnergizante (Cafe "capuchino" _) = True
+esEnergizante (Gaseosa "pomelo" cantAzucar) = cantAzucar > 10
+esEnergizante _  = False
+
+cantidadAzucar :: Bebida -> Azucar
+cantidadAzucar (Cafe _ cantidad) = cantidad
+cantidadAzucar (Gaseosa _ cantidad) = cantidad
+
+data Persona = Estudiante {nom :: String, edad :: Int} deriving Show
+
+cumplirAños :: Persona -> Persona
+cumplirAños estudiante = estudiante {edad = edad estudiante + 1}
+
+julia :: Persona
+julia = Estudiante "julia" 20
+
